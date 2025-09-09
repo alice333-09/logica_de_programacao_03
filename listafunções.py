252b93ea9ee67ae33c8144bacdd5d915a8fe80dc
dm201085 <debora.dantas10@aluno.ce.gov.br>
2025-09-09 11:01:11 -0300

    primeiras questões feitas

---
+++ listafunções.py
@@ -0,0 +1,45 @@
+# quest.1
+
+# def saudação( nome ):
+#     return (f"olá, {nome}! Sejá bem-vindo(a) au curso de lógica de programação.")
+# print (saudação (input("qual seu nome? ")))
+
+# quest.2
+
+# def número (x):
+#     if x % 2 == 0 :
+#         return ("esse número é par")
+#     else:
+#         return ("esse número é impar")
+# print (número (int(input("digite um número: "))))
+
+# quest.3
+
+# x= int(input("digite um número: "))
+# y= int(input("digite outro número: "))
+
+# if x > y :
+#     print (f"{x} é maior que {y}")
+# elif x < y :
+#     print (f"{x} é menor que {y}")
+# else:
+#     print ("esses números são iguais")
+
+# quest.4
+
+# nmr= int(input("digite um número inteiro: "))
+# for i in range (10):
+#     resul= nmr*i
+#     print(f"{nmr} x {i} = {resul}")
+
+# quest.5
+
+# cont= 1
+# while cont <= 10:
+#     print(cont)
+#     cont += 1
+# print("Explosão!!!!!!!!!!!")
+
+# quest.6
+
+
