c787526fe331dfc84028f52ace81036c73914f34
rhavss <puupi169@gmail.com>
2025-10-14 11:48:07 -0300

    icaro

--- Biblioteca.py
+++ Biblioteca.py
@@ -1,25 +1,7 @@
-
-import requests
-import json
-
+import requests, json , random
+sorteio = ["babado do(a)" , "az area do(a)" , "babilonia do(a)" , "casa do carai do(a)" , "localizao do (a)"]
+computador = random.choice(sorteio)
 nome = input("Digite seu nome: ")
-cep = int(input("Digite seu CEP: "))
-dados = requests.get (f"https://viacep.com.br/ws/{cep}/json/") 
-with open(f"{nome}-cep.json" , "w") as ceppessoa:
-    ceppessoa.write(print(f"{dados.json()}"))
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+dados = requests.get(f"https://viacep.com.br/ws/{input('Digite seu CEP: ')}/json/").json()
+print(dados)
+with open(f"{computador} {nome}.json" , "w") as ceppessoa: json.dump(dados, ceppessoa)
