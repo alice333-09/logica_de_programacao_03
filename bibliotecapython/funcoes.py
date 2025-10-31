64cbde601ca657410dba6cc970924b9ab449dde0
pereiradevel <lucaslemoss608@gmail.com>
2025-10-31 08:03:47 -0300

    aliceteca

--- bibliotecapython/funcoes.py
+++ bibliotecapython/funcoes.py
@@ -1,8 +1,53 @@
-def adicionar_livro(ListaLivros):
-    ListaLivros.append(dict)
-def emprestar_livro(ListaLivros):
-    sl = status[1]
-def devolver_livro(ListaLivros):
+import tabulate
+def adicionar_livro(ListaLivros, status):
+    titulo = input("Digite o titulo do Livro: ")
+    autor = input("Digite o autor do livro: ")
     sl = status[0]
+    dict_livro = {"Titulo": titulo, "Autor": autor, "Status": sl} 
+    ListaLivros.append(dict_livro)
+    print("Livro adicionado.")
+
 def exibir_livros(ListaLivros):
-    return ListaLivros
+    if not ListaLivros:
+        print("Nenhum livro cadastrado.")
+    else:
+        headers = ["Título", "Autor", "Status"]
+
+        tabela = [list(livro.values()) for livro in ListaLivros]
+        print(tabulate.tabulate(tabela, headers=headers, tablefmt="fancy_grid"))
+
+def emprestar_livro(ListaLivros, status):
+    livro_titulo = input("Digite o titulo do livro que deseja emprestar: ")
+    livro_encontrado = None
+
+    for livro in ListaLivros:
+        if livro["Titulo"] == livro_titulo:
+            livro_encontrado = livro
+            break
+
+    if livro_encontrado:
+        if livro_encontrado["Status"] == status[0]:
+            livro_encontrado["Status"] = status[1]
+            print(f"{livro_titulo} Emprestado com sucesso!")
+        else:
+            print(f"{livro_titulo} ja esta emprestado.")
+    else:
+        print(f"Livro '{livro_titulo}' nao encontrado.")
+
+def devolver_livro(ListaLivros, status):
+    livro_titulo = input("Digite o titulo do livro que deseja devolver: ")
+    livro_encontrado = None
+
+    for livro in ListaLivros:
+        if livro["Titulo"] == livro_titulo:
+            livro_encontrado = livro
+            break
+
+    if livro_encontrado:
+        if livro_encontrado["Status"] == status[1]:
+            livro_encontrado["Status"] = status[0]
+            print(f"{livro_titulo} Devolvido com sucesso!")
+        else:
+            print(f"{livro_titulo} ja esta disponivel.")
+    else:
+        print(f"Livro '{livro_titulo}' nao encontrado.")
--- bibliotecapython/programa.py
+++ bibliotecapython/programa.py
@@ -1,33 +1,37 @@
 
 import tabulate
+from funcoes import *
+
 ListaLivros = []
-status = ["Disponivel" , "Emprestado"]
+status = ["Disponivel", "Emprestado"]
+
 while True:
     print("1 - Adicionar Livro")
     print("2 - Exibir livros")
     print("3 - Emprestar Livro")
     print("4 - Devolver Livro")
     print("0 - Sair")
-    dgt = int(input("Digite uma opção: "))
+
+
+    dgt = int(input("Digite uma opcao: "))
+
+
     if dgt == 1:
-        titulo = input("Digite o titulo do Livro: ")
-        autor = input("Digite o autor do livro: ")
-        sl = status[0]
-        dict = {
-        "Titulo:" : titulo,
-        "Autor:" : autor,
-        "Status:" : sl
-        }
-        ListaLivros.append(dict)
-        print("Livro adicionado.")
-    if dgt == 2:
-        print(ListaLivros)
-    if dgt == 3:
-        livro = input("Digite o titulo do livro: ")
-        print(f"{livro} Emprestado com sucesso!")
-        sl = status[1]
-    if dgt == 4:
-        livro = input("Digite o titulo do livro: ")
-        print(f"{livro} Devolvido com sucesso!")
-        sl = status[0]
+        adicionar_livro(ListaLivros, status)
+
+    elif dgt == 2:
+        exibir_livros(ListaLivros)
+
+    elif dgt == 3:
+        emprestar_livro(ListaLivros, status)
+
+    elif dgt == 4:
+        devolver_livro(ListaLivros, status)
+            
+    elif dgt == 0:
+        print("Saindo do programa.")
+        break
+
+    else:
+        print("Opcao invalida.")
     
