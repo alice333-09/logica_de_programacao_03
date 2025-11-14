d2efaef5380af915e3b1d7bd19491585c18daffd
pereiradevel <eduardaamaria2608@gmail.com>
2025-11-14 11:55:36 -0300

    pdf

---
+++ cardapio e pedidos/funcao.py
@@ -0,0 +1,25 @@
+
+def crregr_crdpio():
+    Cardapio = [
+        {"id": 1, "nome": "Hambúrguer", "preco": 12.5},
+        {"id": 2, "nome": "Pizza", "preco": 30},
+        {"id": 3, "nome": "Refrigerante", "preco": 5}
+    ]
+    print(Cardapio)
+# exibir_crdpio(crdpio)
+# Exibe o cardápio com tbulte.
+# dicionr_pedido(crdpio, pedido)
+# · Solicita ID do item
+# · Solicita quantidade
+# · Adiciona ao pedido no formato:
+# ·
+# {
+# "item": "Hambúrguer",
+# "qtd": 2,
+# "total": 25.0
+# }
+# exibir_pedido(pedido)
+# · Exibe todos os itens do pedido
+# · Mostra o totl d compr
+# remover_item(pedido)
+# · Remove item pelo nome ou ID
